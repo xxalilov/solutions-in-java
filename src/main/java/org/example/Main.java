1875984e9af66947c47068555acbc67ef427f2af
xxalilov <xolbekxalilov@gmail.com>
2024-02-13 16:53:17 +0500

    add two sum solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = TwoSum.twoSum(nums, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
     }
 }
